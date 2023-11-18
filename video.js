11d7a2c70eeef29b916b8a3e06aecd21c14f3eef
Lea Roncero LAPTOP <lea.roncerop10@gmail.com>
2023-11-18 16:35:58 +0100

    fixed bug: duplicated boxes, todo: animation, json positionEnd

--- video.js
+++ video.js
@@ -5,10 +5,40 @@ function addBox(building, container, start) {
     boxDiv.setAttribute("data-start", start);
     boxDiv.setAttribute("data-end", start + building.duration);
     boxDiv.id = building.buildingId;
-    boxDiv.style.left = building.position.x + "px";
-    boxDiv.style.top = building.position.y + "px";
-    boxDiv.style.width = building.position.width + "px";
-    boxDiv.style.height = building.position.height + "px";
+    boxDiv.style.left = building.positionStart.x + "px";
+    boxDiv.style.top = building.positionStart.y + "px";
+    boxDiv.style.width = building.positionStart.width + "px";
+    boxDiv.style.height = building.positionStart.height + "px";
+
+    // Calculate the difference in X and Y positions
+    const deltaX = building.positionEnd.x - building.positionStart.x;
+    const deltaY = building.positionEnd.y - building.positionStart.y;
+
+    // Calculate the scaling factor (assuming the size of the container changes)
+    const startWidth = building.positionStart.width;
+    const startHeight = building.positionStart.height;
+    const endWidth = building.positionEnd.width;
+    const endHeight = building.positionEnd.height;
+
+    const scaleX = endWidth / startWidth;
+    const scaleY = endHeight / startHeight;
+
+    // Update the animation keyframes
+    const keyframes = `
+        @keyframes moveContainer_${building.buildingId} {
+            from {
+                transform: translate(0, 0) scale(1);
+            }
+            to {
+                transform: translate(${deltaX}px, ${deltaY}px) scale(${scaleX}px, ${scaleY}px);
+            }
+        }
+    `;
+
+    document.styleSheets[1].insertRule(keyframes, 0);
+
+    // Update the animation class for the box
+    boxDiv.style.animation = `moveContainer_${building.buildingId} linear infinite`;
 
     container.appendChild(boxLink);
     boxLink.appendChild(boxDiv);
@@ -25,4 +55,3 @@ function updateInfoBox(infoBox, description, title) {
 function createElement(elementType) {
     return document.createElement(elementType);
 }
-
